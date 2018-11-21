9cbbea36928f45ff602f19d2f38bc12486391868
Mehul Agrawal <agrawal.mehul92@gmail.com>
2018-11-20 16:53:48 -0800

    Updating after review
    game timer function, updateScore function

--- js/app.js
+++ js/app.js
@@ -1,25 +1,28 @@
 /*
  * Create a list that holds all of your cards
  */
- var all_cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"];
- var first_card;
- var score_counter = 0;
- var time_keeper = new Date();
- var match_counter = 0;
- var stars_counter = 3;
- var lock_game = false;
- var time_in_seconds, time_in_minutes;
+ let all_cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"];
+ let first_card;
+ let score_counter = 0;
+ let time_keeper = new Date();
+ let match_counter = 0;
+ let stars_counter = 3;
+ let lock_game = false;
+ let time_in_seconds, time_in_minutes;
+ let game_ended = false;
 
 // Game Timer
- var game_time = setInterval(function() {
-   var update_time = new Date();
-   var time_difference = Math.floor((update_time-time_keeper)/1000);
-   time_in_seconds = time_difference % 60;
-   time_in_minutes = Math.floor(time_difference / 60);
-   if (time_in_seconds < 10) {time_in_seconds = "0" + time_in_seconds;}
-   if (time_in_minutes < 10) {time_in_minutes = "0" + time_in_minutes;}
-   document.getElementById("seconds").textContent = time_in_seconds;
-   document.getElementById("minutes").textContent = time_in_minutes;
+ let game_time = setInterval(function() {
+   if (game_ended == false ) {
+     let update_time = new Date();
+     let time_difference = Math.floor((update_time-time_keeper)/1000);
+     time_in_seconds = time_difference % 60;
+     time_in_minutes = Math.floor(time_difference / 60);
+     if (time_in_seconds < 10) {time_in_seconds = "0" + time_in_seconds;}
+     if (time_in_minutes < 10) {time_in_minutes = "0" + time_in_minutes;}
+     document.getElementById("seconds").textContent = time_in_seconds;
+     document.getElementById("minutes").textContent = time_in_minutes;
+   }
 }, 1000);
 
 //Click listener for any one of the tiles
@@ -27,8 +30,6 @@
      if (evt.target.classList.contains('match') == false && !(evt.target == first_card) && evt.target.classList.contains('card') && !lock_game) {  // ← verifies that clicked card is not already matched or open and more than two cards are not open
        lock_game = true;
        let selectedCard = evt.target;
-       score_counter++;
-       updateScore(score_counter);
        gameTracker(selectedCard);
      }
  });
@@ -42,8 +43,10 @@
 function gameTracker(clickedCard) {
   clickedCard.classList.add("open", "show");
   if (first_card) { //Checking that first card is already open and user clicking on the second card in pair
-    var subCard = first_card.getElementsByTagName('I');
-    var subCard2 = clickedCard.getElementsByTagName('I');
+    score_counter++;
+    updateScore(score_counter);
+    let subCard = first_card.getElementsByTagName('I');
+    let subCard2 = clickedCard.getElementsByTagName('I');
     if (compareCards(subCard, subCard2)){  // If first and second card match
       first_card.classList.add("match");
       clickedCard.classList.add("match");
@@ -70,12 +73,10 @@ function compareCards(subCard, subCard2) {
 // Update the score after every click
 function updateScore(score) {
   document.getElementsByClassName("moves")[0].textContent = score;
-  if (score == 20) {
+  if (score == 12) {
     document.getElementById("star-3").classList.replace("fa-star","fa-star-o"); stars_counter = 2;
-  } else if (score == 25){
+  } else if (score == 16){
     document.getElementById("star-2").classList.replace("fa-star","fa-star-o"); stars_counter = 1;
-  } else if (score == 30) {
-    document.getElementById("star-1").classList.replace("fa-star","fa-star-o"); stars_counter = 0;
   } else if (score == 0) {
     document.getElementById("star-3").classList.replace("fa-star-o","fa-star");
     document.getElementById("star-2").classList.replace("fa-star-o","fa-star");
@@ -95,6 +96,7 @@ function notMatched(first_card, clickedCard) {
 function wonGame() {
   $('#winningModal').modal('toggle');
   current_time = new Date();
+  game_ended = true;
   document.getElementById("move-count").textContent = score_counter;
   document.getElementById("star-count").textContent = stars_counter;
   document.getElementById("game-time-seconds").textContent = time_in_seconds;
@@ -104,14 +106,14 @@ function wonGame() {
 // Restart game
 function restartGame() {
   lock_game = false;
-  var old_order = [];
-  for (var i = 0; i < all_cards.length; i++) {
+   old_order = [];
+  for (let i = 0; i < all_cards.length; i++) {
     old_order[i] = all_cards[i];
   }
   shuffle(all_cards);
-  var blah2 = document.querySelectorAll('.card')
-  var blah = document.querySelectorAll('.card .fa')
-  for (var i = 0; i < all_cards.length; i++) {
+  let blah2 = document.querySelectorAll('.card')
+  let blah = document.querySelectorAll('.card .fa')
+  for (let i = 0; i < all_cards.length; i++) {
     blah2[i].classList.remove("match", "open", "show", "error");
     blah[i].classList.replace(old_order[i], all_cards[i]);
   }
@@ -120,6 +122,7 @@ function restartGame() {
   updateScore(score_counter);
   first_card = null;
   time_keeper = new Date();
+  game_ended = false;
 }
 
 /*
@@ -131,7 +134,7 @@ function restartGame() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
